6c82a03a1c64b81892721f1121bf4d771e168024
Sungwoo Park <swpark@nexell.co.kr>
2014-04-29 11:01:08 +0900

    [MOD] add selinux option to pyxis android defconfig
    [REVERT] patch.c to org

--- arch/arm/kernel/patch.c
+++ arch/arm/kernel/patch.c
@@ -38,10 +38,7 @@ void __kprobes __patch_text(void *addr, unsigned int insn)
 		else
 			insn = __opcode_to_mem_arm(insn);
 
-        // psw0523 block this because of kernel panic
-#if 0
 		*(u32 *)addr = insn;
-#endif
 		size = sizeof(u32);
 	}
 
