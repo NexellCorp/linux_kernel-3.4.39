36d7ca026232dccb8b30b31ef4ea73d400931047
Youngbok Park <ybpark@nexell.co.kr>
2014-05-14 20:49:12 +0900

    device.c

--- arch/arm/plat-nxp4330/pyxis/include/cfg_main.h
+++ arch/arm/plat-nxp4330/pyxis/include/cfg_main.h
@@ -173,7 +173,7 @@
 #define CFG_SPI1_CS_GPIO_MODE					1		/* 0 FSS CONTROL, 1: CS CONTRO GPIO MODE */
 #define CFG_SPI2_CS_GPIO_MODE					0	/* 0 FSS CONTROL, 1: CS CONTRO GPIO MODE */
 
-#define CFG_SPIi0_CS							PAD_GPIO_C + 30	/* 0 FSS CONTROL, 1: CS CONTRO GPIO MODE */
+#define CFG_SPI0_CS							PAD_GPIO_C + 30	/* 0 FSS CONTROL, 1: CS CONTRO GPIO MODE */
 /*------------------------------------------------------------------------------
  *  MPEGTSIF
  */
