b18dd090cea9766d18905f52bb424f4878721d12
jhkim <jhkim@nexell.co.kr>
2014-06-16 20:03:49 +0900

    ADD: clock rate update api

--- arch/arm/mach-nxp4330/include/mach/platform.h
+++ arch/arm/mach-nxp4330/include/mach/platform.h
@@ -84,6 +84,7 @@ void 	 		nxp_board_base_init(void);			/* No init section for suspend */
 
 void 			nxp_cpu_clock_resume(void);
 void			nxp_cpu_clock_print(void);
+void 			nxp_cpu_clock_update_rate(int pll);
 unsigned int 	nxp_cpu_clock_hz(int type);			/* 0=PLL0, 1=PLL1, 2=FCLK, 3=MCLK, 4=BCLK, 5=PCLK */
 void 		 	nxp_cpu_shutdown(void);
 void 		 	nxp_cpu_core_shutdown(int core);
