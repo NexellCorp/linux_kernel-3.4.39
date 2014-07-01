e214a1d506dd4fa7a096373367d653bbdf554f96
Sungwoo Park <swpark@nexell.co.kr>
2014-07-01 16:59:58 +0900

    {FIX] pwm bug for bclk dfs

--- arch/arm/mach-nxp4330/soc/pwm.c
+++ arch/arm/mach-nxp4330/soc/pwm.c
@@ -413,7 +413,7 @@ static int __init nxp_soc_pwm_init(void)
 
 	clk_in_max  = clk_get_rate(clk);
 #if defined(CONFIG_NEXELL_DFS_BCLK)
-	clk_in_max  = 50*1000*1000;
+	clk_in_max  = 20*1000*1000;
 #else
 	clk_in_max /= 2;
 #endif
