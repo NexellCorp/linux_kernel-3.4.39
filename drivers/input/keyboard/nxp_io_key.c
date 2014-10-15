5bf823256a63cc8890e78da63ea799b70a69fb58
jhkim <jhkim@nexell.co.kr>
2014-10-15 13:58:23 +0900

    MOD: prevent resume keyevent

--- drivers/input/keyboard/nxp_io_key.c
+++ drivers/input/keyboard/nxp_io_key.c
@@ -28,41 +28,40 @@
 #include <linux/interrupt.h>
 #include <linux/workqueue.h>
 #include <linux/gpio.h>
-/* nexell soc headers */
+
 #include <mach/platform.h>
 #include <mach/devices.h>
-#include <mach/soc.h>
 
-#if (0)
-#define DBGOUT(msg...)		{ printk(KERN_INFO "key: " msg); }
-#define	ERROUT(msg...)		{ printk(KERN_ERR  "key: " msg); }
-#else
-#define DBGOUT(msg...)		do {} while (0)
-#define	ERROUT(msg...)		do {} while (0)
-#endif
+/*
+#define pr_debug(msg...)		printk(msg)
+*/
 
-#define	KEY_STAT_PRESS		(0)
-#define	KEY_STAT_RELEASE	(1)
-#define	DELAY_WORK_JIFFIES 	(1)
+#define	KEY_STAT_PRESS			(0)
+#define	KEY_STAT_RELEASE		(1)
+#define	DELAY_WORK_JIFFIES 		(1)
+#define	PWR_KEY_RESUME_DELAY 	(500)	/* ms */
 
 struct key_code {
-	struct delayed_work key_event_work;	/* struct work_struct */
-	struct workqueue_struct *key_workqueue;
+	struct delayed_work kcode_work;
+	struct workqueue_struct *kcode_wq;
 	struct key_info *info;
 	unsigned int io;
 	unsigned int keycode;
 	unsigned int val;
 	unsigned int keystat;		/* current detect mode */
 	unsigned int detect_high;	/* detect edge */
+	int irq_disabled;
 };
 
 struct key_info {
 	struct input_dev *input;
 	int	keys;
 	struct key_code *code;
+	struct delayed_work	resume_work;
+	int	resume_delay_ms;
 };
 
-#define	POWER_KEY_MASK	(0x3FC)
+#define	CHECK_PWR_KEY_EVENT(io)		nxp_check_pm_wakeup_dev("power key", io)
 
 struct input_dev *key_input = NULL;
 EXPORT_SYMBOL_GPL(key_input);
@@ -78,19 +77,7 @@ void nxp_key_power_event(void)
 }
 EXPORT_SYMBOL(nxp_key_power_event);
 
-static void nxp_key_setup(struct key_info *key)
-{
-	struct key_code *code = key->code;
-	int num = key->keys ;
-	int i = 0;
-
-	for (; num > i; i++, code++)
-		code->keystat = KEY_STAT_RELEASE;
-
-	key_input = key->input;
-}
-
-static void nxp_key_event(struct work_struct *work)
+static void nxp_key_event_wq(struct work_struct *work)
 {
 	struct key_code *code = (struct key_code *)work;
 	struct key_info *key = code->info;
@@ -109,23 +96,88 @@ static void nxp_key_event(struct work_struct *work)
 	if(press != code->keystat) {
 		code->keystat = press;
 		if (KEY_STAT_PRESS == press) {
-			DBGOUT("%s io:%d, code:%4d DN\n", __func__, code->io, keycode);
 			input_report_key(key->input, keycode, 1);
 			input_sync(key->input);
 		} else {
-			DBGOUT("%s io:%d, code:%4d UP\n", __func__, code->io, keycode);
 			input_report_key(key->input, keycode, 0);
 			input_sync(key->input);
+		}
+		pr_debug("key io:%d, code:%4d %s\n", code->io, keycode,
+			(KEY_STAT_PRESS==press?"DN":"UP"));
+	}
+}
+
+static irqreturn_t nxp_key_irqhnd(int irqno, void *dev_id)
+{
+	struct key_code *code = dev_id;
+
+	queue_delayed_work(code->kcode_wq,
+				&code->kcode_work, DELAY_WORK_JIFFIES);
+
+	return IRQ_HANDLED;
+}
+
+static void nxp_key_resume_work(struct work_struct *work)
+{
+	struct key_info *key = container_of(work, struct key_info, resume_work.work);
+	struct key_code *code = key->code;
+	int i = 0;
+
+	for (i = 0; key->keys > i; i++, code++) {
+		if (code->keycode == KEY_POWER &&
+			code->irq_disabled) {
+			code->irq_disabled = 0;
+			enable_irq(gpio_to_irq(code->io));
+			break;
 		}
 	}
 }
 
-static irqreturn_t nxp_key_interrupt(int irqno, void *dev_id)
+static int nxp_key_suspend(struct platform_device *pdev, pm_message_t state)
 {
-	struct key_code *code = dev_id;
-	queue_delayed_work(code->key_workqueue,
-				&code->key_event_work, DELAY_WORK_JIFFIES);
-	return IRQ_HANDLED;
+	struct key_info *key = platform_get_drvdata(pdev);
+	struct key_code *code = key->code;
+	int i = 0;
+
+	for (i = 0; key->keys > i; i++, code++) {
+		if (code->keycode == KEY_POWER &&
+			code->irq_disabled) {
+			code->irq_disabled = 0;
+			enable_irq(gpio_to_irq(code->io));
+			break;
+		}
+	}
+
+	return 0;
+}
+
+static int nxp_key_resume(struct platform_device *pdev)
+{
+	struct key_info *key = platform_get_drvdata(pdev);
+	struct key_code *code = key->code;
+	int i = 0;
+
+	PM_DBGOUT("%s\n", __func__);
+
+	for (i = 0; key->keys > i; i++, code++) {
+		if (code->keycode == KEY_POWER &&
+			CHECK_PWR_KEY_EVENT(code->io)) {
+
+			int delay = key->resume_delay_ms ? key->resume_delay_ms : PWR_KEY_RESUME_DELAY;
+
+			input_report_key(key->input, KEY_POWER, 1);
+			input_sync(key->input);
+			input_report_key(key->input, KEY_POWER, 0);
+	    	input_sync(key->input);
+
+	    	code->irq_disabled = 1;
+			disable_irq(gpio_to_irq(code->io));
+
+			schedule_delayed_work(&key->resume_work, msecs_to_jiffies(delay));
+		}
+	}
+
+	return 0;
 }
 
 static int nxp_key_probe(struct platform_device *pdev)
@@ -137,45 +189,37 @@ static int nxp_key_probe(struct platform_device *pdev)
 	int i, keys;
 	int ret = 0;
 
-	DBGOUT("%s (device name:%s, id:%d)\n", __func__, pdev->name, pdev->id);
+	pr_debug("%s (device name:%s, id:%d)\n", __func__, pdev->name, pdev->id);
 
-	/*	allocate key_info data */
 	key = kzalloc(sizeof(struct key_info), GFP_KERNEL);
 	if (! key) {
-		printk(KERN_ERR "fail, %s allocate driver info ...\n", pdev->name);
+		pr_err("fail, %s allocate driver info ...\n", pdev->name);
 		return -ENOMEM;
 	}
 
 	keys = plat->bt_count;
 	code = kzalloc(sizeof(struct key_code)*keys, GFP_KERNEL);
-	if (!code) {
-		printk(KERN_ERR "fail, %s key code ...\n", pdev->name);
+	if (NULL == code) {
+		pr_err("fail, %s allocate key code ...\n", pdev->name);
 		ret = -ENOMEM;
-		goto err_mem;
-	}
-
-	for (i = 0; keys > i; i++) {
-		code[i].io = plat->bt_io[i];
-		code[i].keycode = plat->bt_code[i];
-		code[i].detect_high = plat->bt_detect_high ? plat->bt_detect_high[i]: 0;
-		code[i].val = i;
-		code[i].info = key;
-		DBGOUT("%d key [io=%3d, key=%4d]\n", i, code[i].io, code[i].keycode);
+		goto err_mm;
 	}
 
 	input = input_allocate_device();
-	if (!input) {
-		printk(KERN_ERR "fail, %s allocate input device\n", pdev->name);
+	if (NULL == input) {
+		pr_err("fail, %s allocate input device\n", pdev->name);
 		ret = -ENOMEM;
-		goto err_mem;
+		goto err_mm;
 	}
 
-	/* set io info */
 	key->input = input;
 	key->keys = keys;
 	key->code = code;
+	key->resume_delay_ms = plat->resume_delay_ms;
+	key_input = key->input;
 
-	/* set input device info */
+	INIT_DELAYED_WORK(&key->resume_work, nxp_key_resume_work);
+
 	input->name	= "Nexell Keypad";
 	input->phys = "nexell/input0";
 	input->id.bustype = BUS_HOST;
@@ -187,52 +231,63 @@ static int nxp_key_probe(struct platform_device *pdev)
 	input->keycodesize = sizeof(plat->bt_code[0]);
 	input->keycodemax = plat->bt_count * 2;	// for long key
 	input->evbit[0] = BIT_MASK(EV_KEY);
+
 	if (plat->bt_repeat)
 		 input->evbit[0] |= BIT_MASK(EV_REP);
 
 	input_set_capability(input, EV_MSC, MSC_SCAN);
 	input_set_drvdata(input, key);
-	for (i = 0; keys > i; i++)
-		__set_bit(code[i].keycode, input->keybit);
 
 	ret = input_register_device(input);
 	if (ret) {
-		printk(KERN_ERR "fail, %s register for input device ...\n", pdev->name);
-		goto err_mem;
+		pr_err("fail, %s register for input device ...\n", pdev->name);
+		goto err_mm;
 	}
-	nxp_key_setup(key);
 
-	for (i=0; keys > i; i++) {
-		INIT_DELAYED_WORK(&code[i].key_event_work, nxp_key_event);
-	    code[i].key_workqueue = create_singlethread_workqueue(pdev->name);
-		if (!code[i].key_workqueue) {
+	for (i=0; keys > i; i++, code++) {
+		code->io = plat->bt_io[i];
+		code->keycode = plat->bt_code[i];
+		code->detect_high = plat->bt_detect_high ? plat->bt_detect_high[i]: 0;
+		code->val = i;
+		code->info = key;
+		code->keystat = KEY_STAT_RELEASE;
+
+	    code->kcode_wq = create_singlethread_workqueue(pdev->name);
+		if (!code->kcode_wq) {
     	   ret = -ESRCH;
     	   goto err_irq;
 	    }
 
-		ret = request_irq(gpio_to_irq(code[i].io), nxp_key_interrupt,
-				IRQF_SHARED | IRQ_TYPE_EDGE_BOTH, pdev->name, &code[i]);
+		ret = request_irq(gpio_to_irq(code->io), nxp_key_irqhnd,
+					(IRQF_SHARED | IRQ_TYPE_EDGE_BOTH), pdev->name, code);
 		if (ret) {
-			printk(KERN_ERR "fail, gpio[%d] %s request irq...\n",
-				code[i].io, pdev->name);
+			pr_err("fail, gpio[%d] %s request irq...\n", code->io, pdev->name);
 			goto err_irq;
 		}
+
+		__set_bit(code->keycode, input->keybit);
+		INIT_DELAYED_WORK(&code->kcode_work, nxp_key_event_wq);
+		pr_debug("[%d] key io=%3d, code=%4d\n", i, code->io, code->keycode);
 	}
 
 	platform_set_drvdata(pdev, key);
+
 	return ret;
 
 err_irq:
 	for (--i; i >= 0; i--) {
-		cancel_work_sync(&code[i].key_event_work.work);
-	    destroy_workqueue(code[i].key_workqueue);
+		cancel_work_sync(&code[i].kcode_work.work);
+	    destroy_workqueue(code[i].kcode_wq);
 		free_irq(gpio_to_irq(code[i].io), &code[i]);
 	}
 	input_free_device(input);
 
-err_mem:
-	if (code) kfree(code);
-	if (key) kfree(key);
+err_mm:
+	if (code)
+		kfree(code);
+
+	if (key)
+		kfree(key);
 
 	return ret;
 }
@@ -242,72 +297,45 @@ static int nxp_key_remove(struct platform_device *pdev)
 	struct key_info *key = platform_get_drvdata(pdev);
 	struct key_code *code = key->code;
 	int i = 0, irq;
-	DBGOUT("%s\n", __func__);
+
+	pr_debug("%s\n", __func__);
 
 	input_free_device(key->input);
 
 	for (i = 0; i < key->keys; i++) {
-		cancel_work_sync(&code[i].key_event_work.work);
-	    destroy_workqueue(code[i].key_workqueue);
+		cancel_work_sync(&code[i].kcode_work.work);
+	    destroy_workqueue(code[i].kcode_wq);
 		irq = gpio_to_irq(code[i].io);
 		free_irq(irq, &code[i]);
 	}
 
-	if (code) kfree(code);
-	if (key) kfree(key);
+	if (code)
+		kfree(code);
 
-	return 0;
-}
+	if (key)
+		kfree(key);
 
-static int nxp_key_suspend(struct platform_device *pdev, pm_message_t state)
-{
-	PM_DBGOUT("%s\n", __func__);
-	return 0;
-}
-
-static int nxp_key_resume(struct platform_device *pdev)
-{
-	struct key_info *key = platform_get_drvdata(pdev);
-	struct key_code * code = key->code;
-	int i = 0;
-	PM_DBGOUT("%s\n", __func__);
-
-	for (i = 0; key->keys > i; i++) {
-		unsigned int keycode = code[i].keycode;
-		int io = code[i].io;
-		if (keycode == KEY_POWER) {
-			if (nxp_check_pm_wakeup_dev("power key", io)) {
-				input_report_key(key->input, KEY_POWER, 1);
-		    	input_sync(key->input);
-		    	input_report_key(key->input, KEY_POWER, 0);
-		    	input_sync(key->input);
-				PM_DBGOUT("%s POWER UP\n", __func__);
-			}
-		}
-	}
 	return 0;
 }
 
 static struct platform_driver key_plat_driver = {
-	.probe		= nxp_key_probe,
-	.remove		= nxp_key_remove,
-	.suspend	= nxp_key_suspend,
-	.resume		= nxp_key_resume,
 	.driver		= {
 		.owner	= THIS_MODULE,
 		.name	= DEV_NAME_KEYPAD,
 	},
+	.probe		= nxp_key_probe,
+	.remove		= nxp_key_remove,
+	.suspend	= nxp_key_suspend,
+	.resume		= nxp_key_resume,
 };
 
 static int __init nxp_key_init(void)
 {
-	DBGOUT("%s\n", __func__);
 	return platform_driver_register(&key_plat_driver);
 }
 
 static void __exit nxp_key_exit(void)
 {
-	DBGOUT("%s\n", __func__);
 	platform_driver_unregister(&key_plat_driver);
 }
 
@@ -317,5 +345,4 @@ module_exit(nxp_key_exit);
 MODULE_AUTHOR("jhkim <jhkim@nexell.co.kr>");
 MODULE_DESCRIPTION("Keypad driver for the Nexell board");
 MODULE_LICENSE("GPL");
-MODULE_ALIAS("platform:io keypad");
 
