f0a52bdd612388e6ce49bf6c5fbe0462a686778e
Jongsin Park <pjsin865@nexell.co.kr>
2014-11-25 16:14:00 +0900

    -- regulator : add sysfs interface(store)

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -314,7 +314,35 @@ static ssize_t regulator_uV_show(struct device *dev,
 
 	return ret;
 }
-static DEVICE_ATTR(microvolts, 0444, regulator_uV_show, NULL);
+
+static ssize_t regulator_uV_store(struct device *dev,
+				     struct device_attribute *attr,
+				     const char *buf, size_t size)
+{
+	struct regulator_dev *rdev = dev_get_drvdata(dev);
+	ssize_t ret;
+	int val;
+
+	ret = kstrtoint(buf, 10, &val);
+	if (ret < 0)
+		return ret;
+
+	ret = regulator_check_voltage(rdev, &val, &val);
+	if (ret < 0)
+		return ret;
+
+	ret = regulator_check_consumers(rdev, &val, &val);
+	if (ret < 0)
+		return ret;
+
+	ret = _regulator_do_set_voltage(rdev, val, val);
+	if (ret < 0)
+		return ret;
+
+	return size;
+}
+
+static DEVICE_ATTR(microvolts, 0666, regulator_uV_show, regulator_uV_store);
 
 static ssize_t regulator_uA_show(struct device *dev,
 				struct device_attribute *attr, char *buf)
