a5e1961d9d84eac5828813ad31438640f6b7eff9
jhkim <jhkim@nexell.co.kr>
2015-01-16 14:53:41 +0900

    MOD: bugfix when suspend enter -> suspend_alive

--- arch/arm/mach-s5p6818/pm.c
+++ arch/arm/mach-s5p6818/pm.c
@@ -141,7 +141,7 @@ static int suspend_machine(void)
 		}
 	}
 
-	NX_ALIVE_SetOutputEnable32(mask_bits);
+	NX_ALIVE_SetInputEnable32(mask_bits);
 	NX_ALIVE_SetDetectEnable32(mask_bits);
 	NX_ALIVE_SetInterruptEnable32(mask_bits);
 
