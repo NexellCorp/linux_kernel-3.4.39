60c1ee81dc6a9e8c44d5d970f1e68953c6a25ae5
Jongsin Park <pjsin865@nexell.co.kr>
2015-08-13 23:40:55 +0900

    -- tnn-quickboot : Enable adb.

--- drivers/usb/host/ehci-hcd.c
+++ drivers/usb/host/ehci-hcd.c
@@ -1530,7 +1530,7 @@ err_debug:
 	clear_bit(USB_EHCI_LOADED, &usb_hcds_loaded);
 	return retval;
 }
-#ifdef CONFIG_DEFERRED_INIT_CALL
+#if 0//def CONFIG_DEFERRED_INIT_CALL
 deferred_module_init(ehci_hcd_init);
 #else
 module_init(ehci_hcd_init);
