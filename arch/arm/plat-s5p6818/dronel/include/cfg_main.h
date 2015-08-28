22455d455dd30ad53a900150b1d6633252ca9acd
KOO Bon-Gyu <freestyle@nexell.co.kr>
2015-08-28 15:23:40 +0900

    [FTL] update ver.2015-08-12 (tested s5p6818 svt)
        [EWS] 1. EccErrorTask's priority changed. (low -> middle level)
              2. Bad Block Management: Remove useless MapLogs.
    
        Signed-off-by: Kim Tae-Won <taewon@eastwho.com>
    
    Signed-off-by: KOO Bon-Gyu <freestyle@nexell.co.kr>

--- arch/arm/plat-s5p6818/dronel/include/cfg_main.h
+++ arch/arm/plat-s5p6818/dronel/include/cfg_main.h
@@ -62,6 +62,9 @@
 #define CFG_NAND_ECC_BITS               		40			/* 512 - 4,8,16,24 1024 - 24,40,60 */
 //#define CFG_NAND_ECCIRQ_MODE
 
+/* FTL */
+#define CFG_NAND_FTL_START_BLOCK				0x6000000	/* byte address, Must Be Multiple of 8MB */
+
 /*------------------------------------------------------------------------------
  *	Nand (GPIO)
  */
