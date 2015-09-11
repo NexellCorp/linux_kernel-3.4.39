1e8f6cdd26a629f5deb22f0b7d9066a858feca0f
Hyunseok Jung <hsjung@nexell.co.kr>
2015-09-11 15:45:50 +0900

    [REVERT:TK4418] revert samplerate 44100 => 48000 (audio)

--- arch/arm/plat-s5p4418/tk_avn/include/cfg_main.h
+++ arch/arm/plat-s5p4418/tk_avn/include/cfg_main.h
@@ -135,18 +135,18 @@
 #define	CFG_AUDIO_I2S0_MASTER_MODE				CTRUE	// CTRUE
 #define	CFG_AUDIO_I2S0_TRANS_MODE				0		// 0:I2S, 1:Left 2:Right justified */
 #define	CFG_AUDIO_I2S0_FRAME_BIT				32		// 32, 48
-#define	CFG_AUDIO_I2S0_SAMPLE_RATE				44100
+#define	CFG_AUDIO_I2S0_SAMPLE_RATE				48000
 #define	CFG_AUDIO_I2S0_PRE_SUPPLY_MCLK			1
 
 #define	CFG_AUDIO_I2S1_MASTER_MODE				CTRUE	// CTRUE
 #define	CFG_AUDIO_I2S1_TRANS_MODE				0		// 0:I2S, 1:Left 2:Right justified */
 #define	CFG_AUDIO_I2S1_FRAME_BIT				32		// 32, 48
-#define	CFG_AUDIO_I2S1_SAMPLE_RATE				44100
+#define	CFG_AUDIO_I2S1_SAMPLE_RATE				48000
 #define	CFG_AUDIO_I2S1_PRE_SUPPLY_MCLK			1
 
 #define	CFG_AUDIO_I2S2_MASTER_MODE				CTRUE	// CTRUE
 #define	CFG_AUDIO_I2S2_TRANS_MODE				0		// 0:I2S, 1:Left 2:Right justified */
-#define	CFG_AUDIO_I2S2_FRAME_BIT				48		// 32, 48
+#define	CFG_AUDIO_I2S2_FRAME_BIT				32		// 32, 48
 #define	CFG_AUDIO_I2S2_SAMPLE_RATE				48000
 #define	CFG_AUDIO_I2S2_PRE_SUPPLY_MCLK			0
 
@@ -154,8 +154,8 @@
  * 	Audio SPDIF (TX/RX)
  */
 #define	CFG_AUDIO_SPDIF_TX_HDMI_OUT				CTRUE
-#define	CFG_AUDIO_SPDIF_TX_SAMPLE_RATE			44100
-#define	CFG_AUDIO_SPDIF_RX_SAMPLE_RATE			44100
+#define	CFG_AUDIO_SPDIF_TX_SAMPLE_RATE			48000
+#define	CFG_AUDIO_SPDIF_RX_SAMPLE_RATE			48000
 
 /*------------------------------------------------------------------------------
  * 	I2C
