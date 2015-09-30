7fd134a48d8192bb4939e013580dfb96b6aafa72
Youngbok park <ybpark@nexell.co.kr>
2015-09-30 14:20:19 +0900

    modify s5p4418 	dronel -> drone

--- arch/arm/plat-s5p4418/drone/include/cfg_main.h
+++ arch/arm/plat-s5p4418/drone/include/cfg_main.h
@@ -29,7 +29,7 @@
  * 	System Name
  */
 #define	CFG_SYS_CPU_NAME						"s5p4418"
-#define	CFG_SYS_BOARD_NAME						"s5p4418-drone"
+#define	CFG_SYS_BOARD_NAME						"s5p4418-dronel"
 
 /*------------------------------------------------------------------------------
  * 	BUS config
@@ -173,7 +173,8 @@
  */
 #define CFG_I2C0_CLK							100000
 #define CFG_I2C1_CLK							400000	/* TOUCH */
-#define CFG_I2C2_CLK							100000
+#define CFG_I2C2_CLK							200000
+#define CFG_I2C3_CLK							100000
 
 /*------------------------------------------------------------------------------
  * 	SPI
@@ -214,26 +215,28 @@
 /*------------------------------------------------------------------------------
  * 	SDHC
  */
-#define	CFG_SDMMC2_DETECT_IO					(PAD_GPIO_ALV + 1)	/* external cd */
+#define	CFG_SDMMC0_DETECT_IO					(PAD_GPIO_ALV + 1)	/* external cd */
 
 
 /*------------------------------------------------------------------------------
- * 	NXE2000 PMIC
+ * 	PMIC
  */
-#define CFG_SW_UBC_ENABLE						(1)
-
-/**
- * 0 : GPIO interrupt (CFG_GPIO_PMIC_VUSB_DET)
- * 1 : PMIC interrupt (FVUSBDETSINT)
- */
-#define CFG_USB_DET_FROM_PMIC_INT				(0)
-
-#define CFG_GPIO_OTG_USBID_DET					(PAD_GPIO_D + 16)
-#define CFG_GPIO_OTG_VBUS_DET					(PAD_GPIO_D + 21)
-#define CFG_GPIO_PMIC_VUSB_DET					(PAD_GPIO_ALV + 2)		/* Choice for SW_UBC or Wake-up*/
-#define CFG_GPIO_PMIC_LOWBAT_DET				(-1)//(PAD_GPIO_ALV + 3)		/* Critical low battery detect */
-#define CFG_GPIO_PMIC_INTR						(PAD_GPIO_ALV + 4)
+/* NXE2000 PMIC	*/
+#define CFG_SW_UBC_ENABLE						(1)					/* S/W UBC Check */
+#define CFG_USB_DET_FROM_PMIC_INT				(0)					/* 0 : GPIO interrupt (CFG_GPIO_PMIC_VUSB_DET)		1 : PMIC interrupt (FVUSBDETSINT) */
+#define CFG_GPIO_OTG_USBID_DET					(PAD_GPIO_D + 16)	/* USB ID Deteict */
+#define CFG_GPIO_OTG_VBUS_DET					(PAD_GPIO_D + 21)	/* USB OTG Power Enable */
+#define CFG_GPIO_PMIC_VUSB_DET					(PAD_GPIO_ALV + 2)	/* Choice for SW_UBC or Wake-up*/
+#define CFG_GPIO_PMIC_LOWBAT_DET				(-1)					/* Critical low battery detect */
 #define CFG_PMIC_BAT_CHG_SUPPORT				(1)
+
+/* AXP228 PMIC	*/
+#define	CFG_PMIC_I2_CBUS						3					/* i2c channel */
+#define CFG_BATTERY_CAP							3000					/* Battery Capacity */
+
+/* PMIC Common*/
+#define CFG_GPIO_PMIC_INTR						(PAD_GPIO_ALV + 4)	/* PMIC Interrupt */
+
 
 /*------------------------------------------------------------------------------
  * 	Suspend mode
