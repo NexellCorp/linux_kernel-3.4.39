5997873acb77f07939ae9c4f0442e40e054f208e
Hyunseok Jung <hsjung@nexell.co.kr>
2015-10-01 12:50:17 +0900

    [EDIT:AVN4418/6818] remove cpufreq table to use internal default table value.

--- arch/arm/plat-s5p4418/avn_ref/device.c
+++ arch/arm/plat-s5p4418/avn_ref/device.c
@@ -93,25 +93,9 @@ const u8 g_DispBusSI[3] = {
  */
 #if defined(CONFIG_ARM_NXP_CPUFREQ)
 
-static unsigned long dfs_freq_table[][2] = {
-	{ 1400000, },
-//	{ 1300000, },
-	{ 1200000, },
-//	{ 1100000, },
-	{ 1000000, },
-//	{  900000, },
-	{  800000, },
-	{  700000, },
-	{  600000, },
-	{  500000, },
-	{  400000, },
-};
-
 struct nxp_cpufreq_plat_data dfs_plat_data = {
 	.pll_dev	   	= CONFIG_NXP_CPUFREQ_PLLDEV,
 	.supply_name	= "vdd_arm_1.3V",	//refer to CONFIG_REGULATOR_NXE2000
-	.freq_table	   	= dfs_freq_table,
-	.table_size	   	= ARRAY_SIZE(dfs_freq_table),
 	.max_cpufreq	= 1400*1000,
 	.max_retention  =   20*1000,
 	.rest_cpufreq   =  400*1000,
