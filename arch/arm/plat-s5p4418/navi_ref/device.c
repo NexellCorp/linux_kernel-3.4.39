ab1ba3067f60d48e5182b7e62e7d54ebd72636d6
JongKeun Choi <jkchoi@nexell.co.kr>
2016-07-18 15:48:29 +0900

    [REMOVE]remove miss text for s5p4418_navi_ref

--- arch/arm/plat-s5p4418/navi_ref/device.c
+++ arch/arm/plat-s5p4418/navi_ref/device.c
@@ -1,21 +1,4 @@
 /*
- *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
- *                    Version 2, December 2004
- * 
- * Copyright (C) 2016 Nexell Co., Ltd.
- * Author: Jongkeun, Choi <jkchoi@nexell.co.kr>
- * 
- * Everyone is permitted to copy and distribute verbatim or modified
- * copies of this license document, and changing it is allowed as long
- * as the name is changed.
- * 
- *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
- *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
- * 
- *  0. You just DO WHAT THE FUCK YOU WANT TO.
- */
-
-/*
  * (C) Copyright 2009
  * jung hyun kim, Nexell Co, <jhkim@nexell.co.kr>
  *
