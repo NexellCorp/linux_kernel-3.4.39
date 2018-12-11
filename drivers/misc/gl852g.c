c0468e86ed669f377ce3648aa6958e6e9b592260
Hyunseok Jung <hsjung@nexell.co.kr>
2018-12-11 15:49:26 +0900

    vd_cinema: kernel: do reset of usb hub(gl852g) after ehci loading      - do reset of usb hub(gl852g) after ehci loading      - set usb bus Maxpower to 500mA because of each board's usb hub chip is different.
    Change-Id: I6d949c97e4fb0c65fc24a246fa550dfd5d3708d6
    Signed-off-by: Hyunseok Jung <hsjung@nexell.co.kr>

--- drivers/misc/gl852g.c
+++ drivers/misc/gl852g.c
@@ -3,27 +3,51 @@
 #include <linux/slab.h>
 #include <linux/module.h>
 #include <linux/i2c.h>
+#include <linux/delay.h>
 
 #include <mach/platform.h>
 #include <mach/devices.h>
 #include <mach/soc.h>
 
+struct delayed_work gl852g_late_work;
+
+static void gl852g_reset(void)
+{
+	nxp_soc_gpio_set_out_value(CFG_IO_HUB_PWREN, 1);
+	mdelay(10);
+	nxp_soc_gpio_set_out_value(CFG_IO_HUB_NRST, 1);
+}
+
+static void gl852g_late_reset_work(struct work_struct *work)
+{
+	gl852g_reset();
+}
+
 static int gl852g_probe(struct i2c_client *client, const struct i2c_device_id *id)
 {
 	if (!i2c_check_functionality(client->adapter, I2C_FUNC_SMBUS_BYTE))
 		return -ENODEV;
+
+	INIT_DELAYED_WORK(&gl852g_late_work, gl852g_late_reset_work);
+	schedule_delayed_work(&gl852g_late_work,
+                msecs_to_jiffies(1000));
+	nxp_soc_gpio_set_out_value(CFG_IO_HUB_PWREN, 0);
+	nxp_soc_gpio_set_out_value(CFG_IO_HUB_NRST, 0);
+
 	return 0;
 }
 
 static int gl852g_remove(struct i2c_client *client)
 {
-    kfree(i2c_get_clientdata(client));
-    return 0;
+	kfree(i2c_get_clientdata(client));
+
+	return 0;
 }
 
 static int gl852g_suspend(struct i2c_client *client, pm_message_t mesg)
 {
-	nxp_soc_gpio_set_out_value(CFG_IO_HUB_NRST, 0);		
+	nxp_soc_gpio_set_out_value(CFG_IO_HUB_NRST, 0);
+
 	return 0;
 }
 
@@ -31,6 +55,7 @@ static int gl852g_resume(struct i2c_client *client)
 {
 	// Reset
 	nxp_soc_gpio_set_out_value(CFG_IO_HUB_NRST, 1);
+
 	return 0;
 }
 
@@ -38,12 +63,13 @@ static const struct i2c_device_id gl852g_id[] = {
 	{"gl852g", 0},
 	{ }
 };
+
 MODULE_DEVICE_TABLE(i2c, gl852g_id);
 
 static struct i2c_driver gl852g_driver = {
-	.driver 	= {
+	.driver	= {
 		.owner	= THIS_MODULE,
-		.name 	= "gl852g"
+		.name	= "gl852g"
 	},
 	.id_table	= gl852g_id,
 	.probe		= gl852g_probe,
